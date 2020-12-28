9f09a2a4d33a91b467ad5868b5be0ab5e7cbcdde
wufei <wufeiphil@163.com>
2020-12-28 09:50:21 +0800

    test

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -23,13 +23,22 @@ public class App
 
         System.out.println(Sites);
         System.out.println(Sites.get("jack"));
-        System.out.println(Sites.size());
         System.out.println(Sites.isEmpty());
 
         ArrayList<String> sites =new ArrayList<String>();
         sites.add("goolge");
         System.out.println(sites);
-
+        int a[] =new int[2];
+        try {
+            System.out.println("access element free :"+a[3]);
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Exception thrown :"+e);
+        }
+        finally {
+            a[0]=6;
+            System.out.println("First element value: " +a[0]);
+            System.out.println("The finally statement is executed");
+        }
 
     }
 
